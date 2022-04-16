b3eff9e2655d529d95ace9a7507bdca92fa227b8
Joseph Supples <joseph.craig94@gmail.com>
2022-04-16 13:04:01 -0700

    Added onclick listeners to buttons, displays a toast of buttons' text when clicked

--- app/src/main/java/com/example/josephsproject/MainActivity.kt
+++ app/src/main/java/com/example/josephsproject/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.josephsproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val trafficButton = findViewById<Button>(R.id.button7)
+        trafficButton.setOnClickListener{
+            Toast.makeText(this,"Traffic",Toast.LENGTH_SHORT).show()
+        }
+
+        val moviesButton = findViewById<Button>(R.id.button5)
+        moviesButton.setOnClickListener {
+            Toast.makeText(this,"Movies",Toast.LENGTH_SHORT).show()
+        }
+
+        val parksButton = findViewById<Button>(R.id.button8)
+        parksButton.setOnClickListener {
+            Toast.makeText(this,"Parks",Toast.LENGTH_SHORT).show()
+        }
+
+        val citiesButton = findViewById<Button>(R.id.button4)
+        citiesButton.setOnClickListener {
+            Toast.makeText(this,"Cities",Toast.LENGTH_SHORT).show()
+        }
+
+        val loginButton = findViewById<Button>(R.id.button)
+        loginButton.setOnClickListener {
+            Toast.makeText(this,"Login",Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
